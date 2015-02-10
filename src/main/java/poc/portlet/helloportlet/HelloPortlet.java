eb7d7431cfa6f1cfd454940a7837f47d63375776
Mohammed Ali <mohammed.ali.cs@gmail.com>
2015-02-10 16:39:23 +0200

    Add processAction

--- src/main/java/poc/portlet/helloportlet/HelloPortlet.java
+++ src/main/java/poc/portlet/helloportlet/HelloPortlet.java
@@ -2,6 +2,8 @@ package poc.portlet.helloportlet;
 
 import java.io.IOException;
 
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 import javax.portlet.GenericPortlet;
 import javax.portlet.PortletException;
 import javax.portlet.RenderRequest;
@@ -13,20 +15,28 @@ import javax.portlet.RenderResponse;
  */
 public class HelloPortlet extends GenericPortlet {
 
+	private static int renderCount = 0;
+	private static int actionCount = 0;
+
 	@Override
-	
 	protected void doView(RenderRequest request, RenderResponse response)
 			throws PortletException, IOException {
-		// TODO Auto-generated method stub
-//		super.doView(request, response);
-		response.setContentType("text/html");
-		String user = request.getRemoteUser(); 
-		if (user != null) {
-			response.getWriter().println("Welcome " + user + "!");
+		synchronized (this) {
+			renderCount++;
+		}
 
-		} else {
-			response.getWriter().println("Log in to welcome you ;)");
+		response.getWriter().print(
+				"<form action=" + response.createActionURL() + ">"
+						+ "<p>Render has executed " + renderCount + " times</p>"
+						+ "<p>Action has executed " + actionCount + "times</p>"
+						+ "<input type='submit'/>" + "</form>");
 
+	}
+
+	public void processAction(ActionRequest actionRequest,
+			ActionResponse actionResponse) {
+		synchronized (this) {
+			actionCount++;
 		}
 	}
 
