3e4c1057227a4a4601e275ba4c70286e6d293aab
Mohammed Ali <mohammed.ali.cs@gmail.com>
2015-02-12 11:45:29 +0200

    Add Render Response Namespace

---
+++ src/main/java/poc/portlet/renderresponse/RenderResponseNamespaceTwoPortlet.java
@@ -0,0 +1,40 @@
+package poc.portlet.renderresponse;
+
+import java.io.IOException;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.GenericPortlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+public class RenderResponseNamespaceTwoPortlet extends GenericPortlet {
+
+	public void doView(RenderRequest request, RenderResponse response)
+			throws PortletException, IOException {
+		response.getWriter()
+				.println(
+						"<html>"
+								+ "<head>"
+								+ "<script>"
+								+ "var " + response.getNamespace() + "message = 'Handle Submit, Portlet Two';"
+								+ "function  " + response.getNamespace() + "handleSubmit(){"
+								+ " alert('Handle Submit, The Portlet Is '+" + response.getNamespace() +"message);"
+								+ "}"
+								+ "</script>"
+								+ "</head>"
+								+ "<form action="
+								+ response.createActionURL()
+								+ ">"
+								+ "  <input type='button' value='Click On Me' onclick='"+response.getNamespace()+"handleSubmit()'/>"
+								+ "</form>" + "</html>");
+		
+
+	}
+
+	public void processAction(ActionRequest request, ActionResponse response)
+			throws PortletException, IOException {
+		System.out.println("Handled Request ..");
+	}
+}
