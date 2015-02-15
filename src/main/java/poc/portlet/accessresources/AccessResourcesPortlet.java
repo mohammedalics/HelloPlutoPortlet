18027676eb52a1fab3bb0771132a40a2b560c888
Mohammed Ali <mohammed.ali.cs@gmail.com>
2015-02-15 12:40:41 +0200

    Add Logging

--- src/main/java/poc/portlet/accessresources/AccessResourcesPortlet.java
+++ src/main/java/poc/portlet/accessresources/AccessResourcesPortlet.java
@@ -3,6 +3,7 @@ package poc.portlet.accessresources;
 
 import java.io.IOException;
 import java.io.InputStream;
+import org.apache.log4j.Logger;
 
 import javax.portlet.GenericPortlet;
 import javax.portlet.PortletConfig;
@@ -12,6 +13,8 @@ import javax.portlet.RenderResponse;
 
 public class AccessResourcesPortlet extends GenericPortlet{
 
+    Logger logger = Logger.getLogger(AccessResourcesPortlet.class);
+    
 	 public void init(PortletConfig config) throws PortletException {
 	        super.init(config);
 	    }
@@ -19,6 +22,14 @@ public class AccessResourcesPortlet extends GenericPortlet{
 	    public void doView(RenderRequest request, RenderResponse response) throws PortletException, IOException{
 	        System.out.println("Got File :: "+this.getPortletContext().getResource("/index.jsp"));
 	        System.out.println("Got File :: "+this.getPortletContext().getRealPath("file:///c:/index.jsp"));
+	        
+	        this.getPortletContext().log("Got File :: "+this.getPortletContext().getResource("/index.jsp"));
+	        this.getPortletContext().log("Got File :: "+this.getPortletContext().getRealPath("file:///c:/index.jsp"));
+	        
+	        logger.debug("Got File :: "+this.getPortletContext().getResource("/index.jsp"));
+	        logger.debug("Got File :: "+this.getPortletContext().getRealPath("file:///c:/index.jsp"));
+	        
+	        
 	        InputStream input = this.getPortletContext().getResourceAsStream("/index.jsp");
 	        byte [] bytes = new byte[input.available()];
 	        input.read(bytes, 0, input.available());
